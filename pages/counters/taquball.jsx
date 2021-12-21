ca36bdc3661b43bce5894f9624c2ac739e817c14
eternalmangekyo022 <tibcso0322@gmail.com>
2021-12-21 01:58:26 +0100

    automated commit

--- pages/counters/taquball.jsx
+++ pages/counters/taquball.jsx
@@ -1,12 +1,66 @@
-import { Component } from 'react'
+import { Component, createRef } from 'react'
 import Heading from '../../components/Heading'
 import BackButton from '../../components/BackButton'
 import ResetButton from '../../components/ResetButton'
 
+class MinusButton extends Component {
+    constructor({ player, num, ref }) {
+        super()
+        this.num = num
+        this.ref = ref
+        this.state = {
+            player,
+            classStart: "start",
+        }
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.handleMinusPoints = this.handleMinusPoints.bind(this)
+    }
+
+    handleMinusPoints() {
+        alert("Has to be more than 0")
+    }
+
+    handleOnClick() {
+        this.setState(prev => {
+            if(prev.player.points > 0) {
+                return { player: { points: prev.player.points - 1 } }
+            } else {
+                this.handleMinusPoints()
+            }
+        });
+    }
+
+    componentDidMount() {
+        (async function() {
+            setTimeout(() => this.setState({ classStart: "" }), 1000)
+        }());
+    }
+
+    render() {
+        return(
+            <>
+                <div onClick={ this.handleOnClick } ref={ this.ref } className={ `minus-button ${ this.state.classStart } ${ this.num }` }></div>
+            </>
+        )
+    }
+}
+
+
 export default class Taquball extends Component {
-    constructor() {
+    constructor({  }) {
         super();
-        // this.state = {  };
+        const defaultPlayer = {
+            name: "",
+            points: 0,
+            setWins: 0
+        } 
+        this.state = {
+            player1: defaultPlayer,
+            player2: defaultPlayer
+        };
+
+        this.minusButtonFirst = createRef();
+        this.minusButtonSecond = createRef();
     }
 
     componentDidMount() {
@@ -22,7 +76,11 @@ export default class Taquball extends Component {
                 </Heading>
                 <div className={ "content-wrapper" }>
 
+
                     <div className={ "tri-field-container" }>
+
+                    <MinusButton num={ "first" } ref={ this.minusButtonFirst } player={ this.state.player1 }/>
+                    <MinusButton num={ "second" } ref={ this.minusButtonSecond } player={ this.state.player2 } />
 
                         <div className={ "tri-field-big first" }></div>
                         <div className={ "tri-field-big" }></div>
@@ -32,6 +90,7 @@ export default class Taquball extends Component {
                         </div>
 
                     </div>
+
                 </div>
             </>
         )
