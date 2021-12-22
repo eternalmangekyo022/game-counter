dfcaf233eb8950e3bfea2e162ccc5be4ac58ad4d
eternalmangekyo022 <tibcso0322@gmail.com>
2021-12-22 13:55:45 +0100

    automated commit

--- pages/counters/triball.jsx
+++ pages/counters/triball.jsx
@@ -1,8 +1,7 @@
-import { Component, createRef } from 'react'
+import { Component } from 'react'
 import Heading from '../../components/Heading'
 import BackButton from '../../components/BackButton'
 import NameContainer from '../../components/NameContainer'
-
 
 class Player {
     constructor(_name="", points=0, setWins=0) {
@@ -22,34 +21,48 @@ export default class Taquball extends Component {
             reset: "",
             nameContainer: "",
             classStart: "start",
+            maxPoints: 15
         };
 
         this.handleResetButton = this.handleResetButton.bind(this);
         this.cancelReset = this.cancelReset.bind(this);
         this.confirmReset = this.confirmReset.bind(this);
         this.resetOut = this.resetOut.bind(this);
-        this.handlePlayerPlus = this.handlePlayerPlus.bind(this);
+        this.handlePlusClick = this.handlePlusClick.bind(this);
         this.handleMinusClick = this.handleMinusClick.bind(this);
     }
     
-    handlePlayerPlus(player) {
-        if(this.state[player].points == 14) {
-            if(this.state[player].setWins == 2) {
-                alert(`Set won by ${ this.state[player].name }!`);
-                this.setState({ player1: { points: 0, setWins: 0 }, player2: { points: 0, setWins: 0 } })
+    handlePlusClick(player) {
+        const _player = { ...this.state[player] };
+        const opposite = { ...this.state[player == "player1" ? "player2" : "player1"] }
+        if(_player.points == this.state.maxPoints - 1) {
+            if(_player.setWins == 2) {
+                alert(`Set won by ${ _player.name }!`);
+                _player.points = 0;
+                _player.setWins = 0;
+                opposite.points = 0;
+                opposite.setWins = 0;
             } else {
-                this.setState({ [player]: { points: 0, setWins: this.state[player].setWins + 1 } })
+                _player.points = 0;
+                opposite.points = 0;
+                _player.setWins++;
             }
         } else {
-            this.setState({ [player]: { points: this.state[player].points + 1, setWins: this.state[player].setWins } });
+            _player.points++;
         }
+        this.setState({ [player]: _player, [player == "player1" ? "player2" : "player1"]: opposite })
     }
     
     handleMinusClick(player) {
-        const { points } = this.state[player];
+        const _player = { ...this.state[player] }
+        const { points, setWins } = _player;
+        if(points) _player.points--
+        else if(setWins) {
+            _player.setWins--;
+            _player.points = this.state.maxPoints - 1;
+        }
+        this.setState({ [player]: _player });
 
-        if(points) this.setState({ [player]: { points: points - 1, setWins: this.state[player].setWins } })
-        
     }
 
     cancelReset() {
@@ -95,6 +108,9 @@ export default class Taquball extends Component {
                 <div className={ "content-wrapper" }>
 
                     <div className={ "tri-field-container" }>
+                        <NameContainer player={ this.state.player1 } classNum={ "first" } ></NameContainer>
+                        <NameContainer player={ this.state.player2 } classNum={ "second" } ></NameContainer>
+
                         <div className={ "player-set-container" }>
                             <div className={ "player-set first" }>{ this.state.player1.setWins }</div>
                             <div className={ "player-set second" }>{ this.state.player2.setWins }</div>
@@ -103,13 +119,8 @@ export default class Taquball extends Component {
                         <div onClick={ e => this.handleMinusClick("player1") } className={ `minus-button first ${ this.state.classStart }` }></div>
                         <div onClick={ e => this.handleMinusClick("player2") } className={ `minus-button second ${ this.state.classStart }` }></div>
 
-
-                        <div onClick={ e => { this.handlePlayerPlus("player1") } } className={ "tri-field-big" }>
-                            <NameContainer player={ this.state.player1 } classNum={ "first" } ></NameContainer>
-                        </div>
-                        <div onClick={ e => { this.handlePlayerPlus("player2") } } className={ "tri-field-big" }>
-                            <NameContainer player={ this.state.player2 } classNum={ "second" } ></NameContainer>
-                        </div>
+                        <div onClick={ e => { this.handlePlusClick("player1") } } className={ "tri-field-big" }></div>
+                        <div onClick={ e => { this.handlePlusClick("player2") } } className={ "tri-field-big" }></div>
 
                         <div className={ "tri-field-middle" }>
                             <div className={ "player-point-container first" }><span>{ this.state.player1.points }</span></div>
